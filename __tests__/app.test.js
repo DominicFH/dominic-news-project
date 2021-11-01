97ba728f154bc34aacadd2a607b5ccb0e2592d60
Dominic <dominicfrancisharris@gmail.com>
2021-11-01 12:49:18 +0000

    Adds testing for GET /api/topics, sets up app.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,25 @@
 const db = require("../db/index.js");
 const testData = require("../db/data/test-data/index.js");
-const { seed } = require("../db/seeds/seed.js");
+const seed = require("../db/seeds/seed.js");
+const request = require("supertest");
+const app = require("../app");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe("GET /api/topics", () => {
+	it("status 200: responds with an array of topic objects which have a slug and description property", () => {
+		return request(app)
+			.get("/api/topics")
+			.expect(200)
+			.then(({ body }) => {
+				expect(body["topics"].length).toBe(3);
+				body["topics"].forEach((topic) => {
+					expect(topic).toEqual({
+						description: expect.any(String),
+						slug: expect.any(String),
+					});
+				});
+			});
+	});
+});
